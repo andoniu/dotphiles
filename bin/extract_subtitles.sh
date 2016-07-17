af8828ea70a51e433f07793c4ed094424a86b5f2
Ovidiu Andoniu <ovidiu.andoniu@movidius.com>
2016-07-17 11:42:43 +0300

    how to reencode

--- bin/extract_subtitles.sh
+++ bin/extract_subtitles.sh
@@ -9,3 +9,4 @@ for file in $list
         subid=$(mkvmerge -i "$file" | grep subtitles | awk -F" " '{print $3}')
         mkvextract tracks $file $subid$(echo $file | awk -F".mkv" '{print $1}').srt
     done
+#ffmpeg -i Game.Of.Thrones.S06E09.Battle.Of.The.Bastards.Web-DL.1080p.10bit.5.1.x265.HEVC-Qman\[UTR\].Requested.mkv -vcodec copy -acodec copy got_s06e09.mp4
